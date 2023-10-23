8349cd3f71b15b83484a60cb5feb787bf0c95afd
Alex Mamani <97176969+AlexRomanMamani@users.noreply.github.com>
2023-10-23 19:48:28 -0300

    Update lun102323.py

--- Ademar/lun102323.py
+++ Ademar/lun102323.py
@@ -1,11 +1,24 @@
 class Humano():
-    def __init__(self):
-        self.edad = 30
+    def __init__(self, edad):
+        self.edad = edad
         print("yo soy una parte de un objeto.")
     def hablar(self, mensaje):
+        print(self.edad)
         print(mensaje)
-        
-pedro = Humano()
+
+# HERENCIA
+class IngSistema(Humano):
+    def programador(self,lenguaje):
+        print("Ustedes estan programando en: ", lenguaje)
+
+pedro = Humano(22)
+
 algo = input("Ingrese mensaje: ")
+
 pedro.hablar(algo)
+
 print(pedro.edad)
+
+juan = IngSistema(22)
+juan.programador("Python")
+print("Juan tiene : {}".format(juan.edad))
